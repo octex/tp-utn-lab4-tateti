b30a3a0767ee65beadbc23fbfcb627dc808d0b04
octex <56984557+octex@users.noreply.github.com>
2022-10-01 00:50:15 -0300

    Created playersManager class

--- game.js
+++ game.js
@@ -41,13 +41,41 @@ class Player
     }
 }
 
-class Game
+class PlayersManager
 {
     constructor()
     {
         this.player1 = null;
         this.player2 = null;
-        this.currentPlayerTurn = null;
+        this.ready = false;
+    }
+
+    addPlayers()
+    {
+        let player1Name = document.getElementById("player1Name").value;
+        let player2Name = document.getElementById("player2Name").value;
+        //TODO: Validar que en los campos no vengan strings vacios
+        this.addPlayer1(player1Name);
+        this.addPlayer2(player2Name);
+        this.ready = true;
+    }
+
+    addPlayer1(playerName)
+    {
+        this.player1 = new Player(playerName, 'X');
+    }
+
+    addPlayer2(playerName)
+    {
+        this.player2 = new Player(playerName, 'O');
+    }
+}
+
+class Game
+{
+    constructor(playerManager)
+    {
+        this.playerManager = playerManager;
         this.timer = new Timer();
         this.gameBoard = document.getElementById("game");
         this.menu = document.getElementById("menu");
